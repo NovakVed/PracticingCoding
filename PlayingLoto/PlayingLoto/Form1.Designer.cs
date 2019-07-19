8599ec5d08d286e9a3e1bd181057249fd8a35621
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2019-07-19 14:45:31 +0200

    PlayingLoto
    Aplication that let's you simulate playing Loto 7/39

---
+++ PlayingLoto/PlayingLoto/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace PlayingLoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSedmiBroj = new System.Windows.Forms.TextBox();
+            this.txtSestiBroj = new System.Windows.Forms.TextBox();
+            this.txtPetiBroj = new System.Windows.Forms.TextBox();
+            this.txtCetvrtiBroj = new System.Windows.Forms.TextBox();
+            this.txtTreciBroj = new System.Windows.Forms.TextBox();
+            this.txtDrugiBroj = new System.Windows.Forms.TextBox();
+            this.txtPrviBroj = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtIzvuceni7 = new System.Windows.Forms.TextBox();
+            this.txtIzvuceni3 = new System.Windows.Forms.TextBox();
+            this.txtIzvuceni6 = new System.Windows.Forms.TextBox();
+            this.txtIzvuceni1 = new System.Windows.Forms.TextBox();
+            this.txtIzvuceni5 = new System.Windows.Forms.TextBox();
+            this.txtIzvuceni2 = new System.Windows.Forms.TextBox();
+            this.txtIzvuceni4 = new System.Windows.Forms.TextBox();
+            this.lblBrojPogodenih = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOdigraj = new System.Windows.Forms.Button();
+            this.btnUplati = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSedmiBroj);
+            this.groupBox1.Controls.Add(this.txtSestiBroj);
+            this.groupBox1.Controls.Add(this.txtPetiBroj);
+            this.groupBox1.Controls.Add(this.txtCetvrtiBroj);
+            this.groupBox1.Controls.Add(this.txtTreciBroj);
+            this.groupBox1.Controls.Add(this.txtDrugiBroj);
+            this.groupBox1.Controls.Add(this.txtPrviBroj);
+            this.groupBox1.Location = new System.Drawing.Point(29, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(447, 144);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Uneseni brojevi";
+            // 
+            // txtSedmiBroj
+            // 
+            this.txtSedmiBroj.Location = new System.Drawing.Point(300, 49);
+            this.txtSedmiBroj.Name = "txtSedmiBroj";
+            this.txtSedmiBroj.Size = new System.Drawing.Size(34, 22);
+            this.txtSedmiBroj.TabIndex = 6;
+            // 
+            // txtSestiBroj
+            // 
+            this.txtSestiBroj.Location = new System.Drawing.Point(260, 49);
+            this.txtSestiBroj.Name = "txtSestiBroj";
+            this.txtSestiBroj.Size = new System.Drawing.Size(34, 22);
+            this.txtSestiBroj.TabIndex = 5;
+            // 
+            // txtPetiBroj
+            // 
+            this.txtPetiBroj.Location = new System.Drawing.Point(220, 49);
+            this.txtPetiBroj.Name = "txtPetiBroj";
+            this.txtPetiBroj.Size = new System.Drawing.Size(34, 22);
+            this.txtPetiBroj.TabIndex = 4;
+            // 
+            // txtCetvrtiBroj
+            // 
+            this.txtCetvrtiBroj.Location = new System.Drawing.Point(180, 49);
+            this.txtCetvrtiBroj.Name = "txtCetvrtiBroj";
+            this.txtCetvrtiBroj.Size = new System.Drawing.Size(34, 22);
+            this.txtCetvrtiBroj.TabIndex = 3;
+            // 
+            // txtTreciBroj
+            // 
+            this.txtTreciBroj.Location = new System.Drawing.Point(140, 49);
+            this.txtTreciBroj.Name = "txtTreciBroj";
+            this.txtTreciBroj.Size = new System.Drawing.Size(34, 22);
+            this.txtTreciBroj.TabIndex = 2;
+            // 
+            // txtDrugiBroj
+            // 
+            this.txtDrugiBroj.Location = new System.Drawing.Point(100, 49);
+            this.txtDrugiBroj.Name = "txtDrugiBroj";
+            this.txtDrugiBroj.Size = new System.Drawing.Size(34, 22);
+            this.txtDrugiBroj.TabIndex = 1;
+            // 
+            // txtPrviBroj
+            // 
+            this.txtPrviBroj.Location = new System.Drawing.Point(60, 49);
+            this.txtPrviBroj.Name = "txtPrviBroj";
+            this.txtPrviBroj.Size = new System.Drawing.Size(34, 22);
+            this.txtPrviBroj.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtIzvuceni7);
+            this.groupBox2.Controls.Add(this.txtIzvuceni3);
+            this.groupBox2.Controls.Add(this.txtIzvuceni6);
+            this.groupBox2.Controls.Add(this.txtIzvuceni1);
+            this.groupBox2.Controls.Add(this.txtIzvuceni5);
+            this.groupBox2.Controls.Add(this.txtIzvuceni2);
+            this.groupBox2.Controls.Add(this.txtIzvuceni4);
+            this.groupBox2.Location = new System.Drawing.Point(29, 197);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(447, 144);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Izvučeni brojevi";
+            // 
+            // txtIzvuceni7
+            // 
+            this.txtIzvuceni7.Location = new System.Drawing.Point(300, 50);
+            this.txtIzvuceni7.Name = "txtIzvuceni7";
+            this.txtIzvuceni7.Size = new System.Drawing.Size(34, 22);
+            this.txtIzvuceni7.TabIndex = 13;
+            // 
+            // txtIzvuceni3
+            // 
+            this.txtIzvuceni3.Location = new System.Drawing.Point(140, 50);
+            this.txtIzvuceni3.Name = "txtIzvuceni3";
+            this.txtIzvuceni3.Size = new System.Drawing.Size(34, 22);
+            this.txtIzvuceni3.TabIndex = 9;
+            // 
+            // txtIzvuceni6
+            // 
+            this.txtIzvuceni6.Location = new System.Drawing.Point(260, 50);
+            this.txtIzvuceni6.Name = "txtIzvuceni6";
+            this.txtIzvuceni6.Size = new System.Drawing.Size(34, 22);
+            this.txtIzvuceni6.TabIndex = 12;
+            // 
+            // txtIzvuceni1
+            // 
+            this.txtIzvuceni1.Location = new System.Drawing.Point(60, 50);
+            this.txtIzvuceni1.Name = "txtIzvuceni1";
+            this.txtIzvuceni1.Size = new System.Drawing.Size(34, 22);
+            this.txtIzvuceni1.TabIndex = 7;
+            // 
+            // txtIzvuceni5
+            // 
+            this.txtIzvuceni5.Location = new System.Drawing.Point(220, 50);
+            this.txtIzvuceni5.Name = "txtIzvuceni5";
+            this.txtIzvuceni5.Size = new System.Drawing.Size(34, 22);
+            this.txtIzvuceni5.TabIndex = 11;
+            // 
+            // txtIzvuceni2
+            // 
+            this.txtIzvuceni2.Location = new System.Drawing.Point(100, 50);
+            this.txtIzvuceni2.Name = "txtIzvuceni2";
+            this.txtIzvuceni2.Size = new System.Drawing.Size(34, 22);
+            this.txtIzvuceni2.TabIndex = 8;
+            // 
+            // txtIzvuceni4
+            // 
+            this.txtIzvuceni4.Location = new System.Drawing.Point(180, 50);
+            this.txtIzvuceni4.Name = "txtIzvuceni4";
+            this.txtIzvuceni4.Size = new System.Drawing.Size(34, 22);
+            this.txtIzvuceni4.TabIndex = 10;
+            // 
+            // lblBrojPogodenih
+            // 
+            this.lblBrojPogodenih.AutoSize = true;
+            this.lblBrojPogodenih.Location = new System.Drawing.Point(150, 360);
+            this.lblBrojPogodenih.Name = "lblBrojPogodenih";
+            this.lblBrojPogodenih.Size = new System.Drawing.Size(32, 17);
+            this.lblBrojPogodenih.TabIndex = 15;
+            this.lblBrojPogodenih.Text = "broj";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Pogođeni brojevi:";
+            // 
+            // btnOdigraj
+            // 
+            this.btnOdigraj.Enabled = false;
+            this.btnOdigraj.Location = new System.Drawing.Point(401, 392);
+            this.btnOdigraj.Name = "btnOdigraj";
+            this.btnOdigraj.Size = new System.Drawing.Size(75, 30);
+            this.btnOdigraj.TabIndex = 2;
+            this.btnOdigraj.Text = "Odigraj";
+            this.btnOdigraj.UseVisualStyleBackColor = true;
+            this.btnOdigraj.Click += new System.EventHandler(this.BtnOdigraj_Click);
+            // 
+            // btnUplati
+            // 
+            this.btnUplati.Location = new System.Drawing.Point(320, 392);
+            this.btnUplati.Name = "btnUplati";
+            this.btnUplati.Size = new System.Drawing.Size(75, 30);
+            this.btnUplati.TabIndex = 3;
+            this.btnUplati.Text = "Uplati";
+            this.btnUplati.UseVisualStyleBackColor = true;
+            this.btnUplati.Click += new System.EventHandler(this.BtnUplati_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 450);
+            this.Controls.Add(this.lblBrojPogodenih);
+            this.Controls.Add(this.btnUplati);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOdigraj);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSedmiBroj;
+        private System.Windows.Forms.TextBox txtSestiBroj;
+        private System.Windows.Forms.TextBox txtPetiBroj;
+        private System.Windows.Forms.TextBox txtCetvrtiBroj;
+        private System.Windows.Forms.TextBox txtTreciBroj;
+        private System.Windows.Forms.TextBox txtDrugiBroj;
+        private System.Windows.Forms.TextBox txtPrviBroj;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblBrojPogodenih;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIzvuceni7;
+        private System.Windows.Forms.TextBox txtIzvuceni6;
+        private System.Windows.Forms.TextBox txtIzvuceni1;
+        private System.Windows.Forms.TextBox txtIzvuceni5;
+        private System.Windows.Forms.TextBox txtIzvuceni2;
+        private System.Windows.Forms.TextBox txtIzvuceni4;
+        private System.Windows.Forms.Button btnOdigraj;
+        private System.Windows.Forms.Button btnUplati;
+        private System.Windows.Forms.TextBox txtIzvuceni3;
+    }
+}
+
---
+++ PlayingLoto/PlayingLoto/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PlayingLoto
+{
+    public partial class Form1 : Form
+    {
+        private Loto loto;
+        public Form1()
+        {
+            InitializeComponent();
+            loto = new Loto();
+        }
+
+        /// <summary>
+        /// Uneseni brojevi se spremaju u listu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnUplati_Click(object sender, EventArgs e)
+        {
+            List<string> vrijednosti = new List<string>();
+            vrijednosti.Add(txtPrviBroj.Text);
+            vrijednosti.Add(txtDrugiBroj.Text);
+            vrijednosti.Add(txtTreciBroj.Text);
+            vrijednosti.Add(txtCetvrtiBroj.Text);
+            vrijednosti.Add(txtPetiBroj.Text);
+            vrijednosti.Add(txtSestiBroj.Text);
+            vrijednosti.Add(txtSedmiBroj.Text);
+            bool ispravnaKombinacija = loto.UnesiUplaceneBrojeve(vrijednosti);
+            if (ispravnaKombinacija == true)
+            {
+                btnOdigraj.Enabled = true;
+            }
+            else
+            {
+                btnOdigraj.Enabled = false;
+                MessageBox.Show("Unijeli ste krive brojeve!");
+            }
+        }
+
+        private void BtnOdigraj_Click(object sender, EventArgs e)
+        {
+            loto.GenerirajDobitneBrojeve();
+
+            txtIzvuceni1.Text = loto.DobitniBrojevi[0].ToString();
+            txtIzvuceni2.Text = loto.DobitniBrojevi[1].ToString();
+            txtIzvuceni3.Text = loto.DobitniBrojevi[2].ToString();
+            txtIzvuceni4.Text = loto.DobitniBrojevi[3].ToString();
+            txtIzvuceni5.Text = loto.DobitniBrojevi[4].ToString();
+            txtIzvuceni6.Text = loto.DobitniBrojevi[5].ToString();
+            txtIzvuceni7.Text = loto.DobitniBrojevi[6].ToString();
+            int brojPogadata = loto.UsporediBrojeve();
+            lblBrojPogodenih.Text = brojPogadata.ToString();
+        }
+    }
+}
