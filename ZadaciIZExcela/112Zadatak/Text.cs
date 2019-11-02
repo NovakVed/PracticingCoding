9857264e4edcd555fc5546485e1698a0ae4acc48
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2019-11-02 15:14:17 +0100

    Zadatak112
    Solved example

--- ZadaciIZExcela/112Zadatak/Text.cs
+++ ZadaciIZExcela/112Zadatak/Text.cs
@@ -1,19 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _112Zadatak
 {
-    class Text
+    internal class Text
     {
         public string InputedWord { get; set; }
-        public List<string> words = new List<string>();
 
-        public Text(string text)
+        public void InputedText(string text)
         {
             InputedWord = text;
+        }
+
+        public string ReverseOrder()
+        {
+            char[] charArray = InputedWord.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public string NumberOfChar()
+        {
+            char[] charArray = InputedWord.ToCharArray();
+            return charArray.Length.ToString();
+        }
+
+        public string NumberOfInputedChar(char c)
+        {
+            char[] charArray = InputedWord.ToCharArray();
+            int counter = 0;
+            if (c.Equals(""))
+            {
+                return counter.ToString();
+            }
+            else
+            {
+                foreach (var item in charArray)
+                {
+                    if (item.Equals(c))
+                    {
+                        counter++;
+                    }
+                }
+                return counter.ToString();
+            }
         }
     }
 }
