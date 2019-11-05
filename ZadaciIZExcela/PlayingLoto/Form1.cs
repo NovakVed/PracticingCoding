55c3fcb837fb33c42e61add7e1c4ca3f6c252c70
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2019-11-06 00:22:52 +0100

    Fix required
    Fix required

--- ZadaciIZExcela/PlayingLoto/Form1.cs
+++ ZadaciIZExcela/PlayingLoto/Form1.cs
@@ -12,9 +12,38 @@ namespace PlayingLoto
 {
     public partial class Form1 : Form
     {
+        private readonly Loto loto;
         public Form1()
         {
             InitializeComponent();
+            loto = new Loto();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            List<int> listaUnesenihBrojeva = new List<int>
+            {
+                int.Parse(textBox1.Text),
+                int.Parse(textBox2.Text),
+                int.Parse(textBox3.Text),
+                int.Parse(textBox4.Text),
+                int.Parse(textBox5.Text),
+                int.Parse(textBox6.Text),
+                int.Parse(textBox7.Text)
+            };
+        }
+
+        private void btnOdigraj_Click(object sender, EventArgs e)
+        {
+            loto.GenerirajDobitneBrojeve();
+            txtIzvuceni1.Text = loto.DobitniBrojevi[0].ToString();
+            txtIzvuceni2.Text = loto.DobitniBrojevi[0].ToString();
+            txtIzvuceni3.Text = loto.DobitniBrojevi[0].ToString();
+            txtIzvuceni4.Text = loto.DobitniBrojevi[0].ToString();
+            txtIzvuceni5.Text = loto.DobitniBrojevi[0].ToString();
+            txtIzvuceni6.Text = loto.DobitniBrojevi[0].ToString();
+            txtIzvuceni7.Text = loto.DobitniBrojevi[0].ToString();
+
         }
     }
 }
--- ZadaciIZExcela/PlayingLoto/Loto.cs
+++ ZadaciIZExcela/PlayingLoto/Loto.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlayingLoto
 {
-    class Loto
+    internal class Loto
     {
         public List<int> UneseniBrojevi { get; set; }
         public List<int> DobitniBrojevi { get; set; }
@@ -19,12 +16,39 @@ namespace PlayingLoto
 
         public void GenerirajDobitneBrojeve()
         {
-
+            Random random = new Random();
+            while (DobitniBrojevi.Count < 7)
+            {
+                int broj = random.Next(1, 39);
+                if (!DobitniBrojevi.Contains(broj))
+                {
+                    DobitniBrojevi.Add(broj);
+                }
+            }
         }
 
-        public void IzracunajDobitneBrojeve()
+        public void UnesenaListaBrojeva(List<int> listaBrojeva)
         {
+            UneseniBrojevi = listaBrojeva; //pogreska! Pogledaj na pdf.u
+        }
 
+        public int IzracunajDobitneBrojeve(List<int> listaBrojeva)
+        {
+            int brojPogodenih = 0;
+            foreach (var item in listaBrojeva)
+            {
+                if (DobitniBrojevi.Contains(item))
+                {
+                    brojPogodenih++;
+                }
+            }
+
+            return brojPogodenih;
+        }
+
+        public void IzbrisiListu()
+        {
+            DobitniBrojevi.Clear();
         }
     }
 }
