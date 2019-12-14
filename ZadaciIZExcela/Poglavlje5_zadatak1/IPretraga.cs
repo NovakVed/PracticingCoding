4e325398c6cd3efa0a251823214f28b7eab0faa7
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2019-12-14 20:32:15 +0100

    Create IPretraga.cs

---
+++ ZadaciIZExcela/Poglavlje5_zadatak1/IPretraga.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poglavlje5_zadatak1
+{
+    public interface IPretraga
+    {
+        bool Usporedi(string kljucnaRijec);
+
+        string Ispis();
+    }
+}
