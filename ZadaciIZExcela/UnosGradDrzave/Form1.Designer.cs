b2f68d46abb0f499762d3fd705a914e178b634e2
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2020-01-27 11:58:03 +0100

    update

--- ZadaciIZExcela/UnosGradDrzave/Form1.Designer.cs
+++ ZadaciIZExcela/UnosGradDrzave/Form1.Designer.cs
@@ -33,38 +33,38 @@
             this.btnUnesiDrzavu = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.cbOdabirDrzave = new System.Windows.Forms.ComboBox();
+            this.cmbOdabirDrzave = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.btnUnosGrada = new System.Windows.Forms.Button();
             this.txtNazivGrada = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.dgwIspisDrzavaGradova = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgwIspisDrzavaGradova)).BeginInit();
+            this.dgvIspisDrzavaGradova = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIspisDrzavaGradova)).BeginInit();
             this.SuspendLayout();
             // 
             // txtNazivDrzave
             // 
-            this.txtNazivDrzave.Location = new System.Drawing.Point(23, 41);
-            this.txtNazivDrzave.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNazivDrzave.Location = new System.Drawing.Point(31, 50);
+            this.txtNazivDrzave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNazivDrzave.Name = "txtNazivDrzave";
-            this.txtNazivDrzave.Size = new System.Drawing.Size(119, 20);
+            this.txtNazivDrzave.Size = new System.Drawing.Size(157, 22);
             this.txtNazivDrzave.TabIndex = 0;
             // 
             // txtBrojStanovnika
             // 
-            this.txtBrojStanovnika.Location = new System.Drawing.Point(260, 76);
-            this.txtBrojStanovnika.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBrojStanovnika.Location = new System.Drawing.Point(347, 94);
+            this.txtBrojStanovnika.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtBrojStanovnika.Name = "txtBrojStanovnika";
-            this.txtBrojStanovnika.Size = new System.Drawing.Size(119, 20);
+            this.txtBrojStanovnika.Size = new System.Drawing.Size(157, 22);
             this.txtBrojStanovnika.TabIndex = 1;
             // 
             // btnUnesiDrzavu
             // 
-            this.btnUnesiDrzavu.Location = new System.Drawing.Point(52, 63);
-            this.btnUnesiDrzavu.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUnesiDrzavu.Location = new System.Drawing.Point(69, 78);
+            this.btnUnesiDrzavu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnUnesiDrzavu.Name = "btnUnesiDrzavu";
-            this.btnUnesiDrzavu.Size = new System.Drawing.Size(88, 42);
+            this.btnUnesiDrzavu.Size = new System.Drawing.Size(117, 52);
             this.btnUnesiDrzavu.TabIndex = 2;
             this.btnUnesiDrzavu.Text = "Unesi drzavu";
             this.btnUnesiDrzavu.UseVisualStyleBackColor = true;
@@ -73,109 +73,107 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 22);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(31, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.Size = new System.Drawing.Size(90, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Naziv drzave";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(260, 59);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(347, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.Size = new System.Drawing.Size(105, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Broj stanovnika";
             // 
-            // cbOdabirDrzave
+            // cmbOdabirDrzave
             // 
-            this.cbOdabirDrzave.FormattingEnabled = true;
-            this.cbOdabirDrzave.Location = new System.Drawing.Point(260, 112);
-            this.cbOdabirDrzave.Margin = new System.Windows.Forms.Padding(2);
-            this.cbOdabirDrzave.Name = "cbOdabirDrzave";
-            this.cbOdabirDrzave.Size = new System.Drawing.Size(119, 21);
-            this.cbOdabirDrzave.TabIndex = 5;
+            this.cmbOdabirDrzave.FormattingEnabled = true;
+            this.cmbOdabirDrzave.Location = new System.Drawing.Point(347, 138);
+            this.cmbOdabirDrzave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbOdabirDrzave.Name = "cmbOdabirDrzave";
+            this.cmbOdabirDrzave.Size = new System.Drawing.Size(157, 24);
+            this.cmbOdabirDrzave.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(260, 22);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(347, 27);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.Size = new System.Drawing.Size(84, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Naziv grada";
             // 
             // btnUnosGrada
             // 
-            this.btnUnosGrada.Location = new System.Drawing.Point(291, 136);
-            this.btnUnosGrada.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUnosGrada.Location = new System.Drawing.Point(388, 167);
+            this.btnUnosGrada.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnUnosGrada.Name = "btnUnosGrada";
-            this.btnUnosGrada.Size = new System.Drawing.Size(88, 42);
+            this.btnUnosGrada.Size = new System.Drawing.Size(117, 52);
             this.btnUnosGrada.TabIndex = 7;
             this.btnUnosGrada.Text = "Unesi grad";
             this.btnUnosGrada.UseVisualStyleBackColor = true;
+            this.btnUnosGrada.Click += new System.EventHandler(this.btnUnosGrada_Click);
             // 
             // txtNazivGrada
             // 
-            this.txtNazivGrada.Location = new System.Drawing.Point(262, 38);
-            this.txtNazivGrada.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNazivGrada.Location = new System.Drawing.Point(349, 47);
+            this.txtNazivGrada.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNazivGrada.Name = "txtNazivGrada";
-            this.txtNazivGrada.Size = new System.Drawing.Size(119, 20);
+            this.txtNazivGrada.Size = new System.Drawing.Size(157, 22);
             this.txtNazivGrada.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(260, 96);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(347, 118);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.Size = new System.Drawing.Size(53, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "Drzava";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(23, 178);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(31, 219);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.Size = new System.Drawing.Size(36, 17);
             this.label5.TabIndex = 11;
             this.label5.Text = "Ispis";
             // 
-            // dgwIspisDrzavaGradova
+            // dgvIspisDrzavaGradova
             // 
-            this.dgwIspisDrzavaGradova.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwIspisDrzavaGradova.Location = new System.Drawing.Point(23, 207);
-            this.dgwIspisDrzavaGradova.Name = "dgwIspisDrzavaGradova";
-            this.dgwIspisDrzavaGradova.Size = new System.Drawing.Size(356, 150);
-            this.dgwIspisDrzavaGradova.TabIndex = 12;
+            this.dgvIspisDrzavaGradova.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIspisDrzavaGradova.Location = new System.Drawing.Point(31, 255);
+            this.dgvIspisDrzavaGradova.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvIspisDrzavaGradova.Name = "dgvIspisDrzavaGradova";
+            this.dgvIspisDrzavaGradova.RowHeadersWidth = 51;
+            this.dgvIspisDrzavaGradova.Size = new System.Drawing.Size(475, 185);
+            this.dgvIspisDrzavaGradova.TabIndex = 12;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 369);
-            this.Controls.Add(this.dgwIspisDrzavaGradova);
+            this.ClientSize = new System.Drawing.Size(544, 454);
+            this.Controls.Add(this.dgvIspisDrzavaGradova);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnUnosGrada);
             this.Controls.Add(this.txtNazivGrada);
-            this.Controls.Add(this.cbOdabirDrzave);
+            this.Controls.Add(this.cmbOdabirDrzave);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnUnesiDrzavu);
             this.Controls.Add(this.txtBrojStanovnika);
             this.Controls.Add(this.txtNazivDrzave);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dgwIspisDrzavaGradova)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIspisDrzavaGradova)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,13 +186,13 @@
         private System.Windows.Forms.Button btnUnesiDrzavu;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cbOdabirDrzave;
+        private System.Windows.Forms.ComboBox cmbOdabirDrzave;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnUnosGrada;
         private System.Windows.Forms.TextBox txtNazivGrada;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DataGridView dgwIspisDrzavaGradova;
+        private System.Windows.Forms.DataGridView dgvIspisDrzavaGradova;
     }
 }
 
--- ZadaciIZExcela/UnosGradDrzave/Form1.cs
+++ ZadaciIZExcela/UnosGradDrzave/Form1.cs
@@ -15,11 +15,76 @@ namespace UnosGradDrzave
         public Form1()
         {
             InitializeComponent();
+            cmbOdabirDrzave.DisplayMember = "Naziv";
+            OsvjeziTablicu();
         }
 
         private void btnUnesiDrzavu_Click(object sender, EventArgs e)
         {
+            if (txtNazivDrzave.Text.Equals(""))
+            {
+                MessageBox.Show("Niste upisali državu!", "Error");
+            }
+            else
+            {
+                Drzava drzava = new Drzava(txtNazivDrzave.Text);
+                DodajUComboBox(drzava);
+                OsvjeziTablicu();
+            }
+        }
 
+        private void DodajUComboBox(Drzava drzava)
+        {
+            cmbOdabirDrzave.Items.Add(drzava);
+        }
+
+        private void btnUnosGrada_Click(object sender, EventArgs e)
+        {
+            string nazivGrada = txtNazivGrada.Text;
+            int brojStanovnika = 0;
+
+            try
+            {
+                brojStanovnika = int.Parse(txtBrojStanovnika.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Niste unijeli broj stanivnika!", "Error");
+                return;
+            }
+
+            Grad grad = new Grad(nazivGrada, brojStanovnika);
+
+            try
+            {
+                Drzava drzava = cmbOdabirDrzave.SelectedItem as Drzava;
+                drzava.ListaGradova.Add(grad);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Niste odabrali drzavu!", "Error");
+                return;
+            }
+            OsvjeziTablicu();
+        }
+
+        private void OsvjeziTablicu()
+        {
+            dgvIspisDrzavaGradova.DataSource = null;
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Dražava", typeof(string));
+            table.Columns.Add("Grad", typeof(string));
+            table.Columns.Add("Stanovika", typeof(int));
+            foreach (var drzava in Drzava.ListaDrzava)
+            {
+                foreach (var grad in drzava.ListaGradova)
+                {
+                    table.Rows.Add(drzava.Naziv, grad.Naziv, grad.BrojStanovnika);
+                }
+            }
+
+            dgvIspisDrzavaGradova.DataSource = table;
         }
     }
 }
