76eb89c75645a14e131b84f640a7f670f4a851e9
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2020-02-07 23:17:16 +0100

    Completed task Imenik

---
+++ ZadaciIZExcela/Imenik/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Imenik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtBroj = new System.Windows.Forms.TextBox();
+            this.cmbGrupa = new System.Windows.Forms.ComboBox();
+            this.lbKontakta = new System.Windows.Forms.ListBox();
+            this.txtPretrazi = new System.Windows.Forms.TextBox();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnNoviKontakt = new System.Windows.Forms.Button();
+            this.btnPretrazi = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kontakti";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Broj";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Grupe";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Pretrazi";
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(121, 29);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(121, 20);
+            this.txtIme.TabIndex = 7;
+            // 
+            // txtBroj
+            // 
+            this.txtBroj.Location = new System.Drawing.Point(121, 72);
+            this.txtBroj.Name = "txtBroj";
+            this.txtBroj.Size = new System.Drawing.Size(121, 20);
+            this.txtBroj.TabIndex = 8;
+            // 
+            // cmbGrupa
+            // 
+            this.cmbGrupa.FormattingEnabled = true;
+            this.cmbGrupa.Location = new System.Drawing.Point(121, 119);
+            this.cmbGrupa.Name = "cmbGrupa";
+            this.cmbGrupa.Size = new System.Drawing.Size(121, 21);
+            this.cmbGrupa.TabIndex = 11;
+            // 
+            // lbKontakta
+            // 
+            this.lbKontakta.FormattingEnabled = true;
+            this.lbKontakta.Location = new System.Drawing.Point(13, 26);
+            this.lbKontakta.Name = "lbKontakta";
+            this.lbKontakta.Size = new System.Drawing.Size(319, 238);
+            this.lbKontakta.TabIndex = 12;
+            // 
+            // txtPretrazi
+            // 
+            this.txtPretrazi.Location = new System.Drawing.Point(70, 307);
+            this.txtPretrazi.Name = "txtPretrazi";
+            this.txtPretrazi.Size = new System.Drawing.Size(178, 20);
+            this.txtPretrazi.TabIndex = 13;
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(121, 174);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(75, 23);
+            this.btnSpremi.TabIndex = 14;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // btnNoviKontakt
+            // 
+            this.btnNoviKontakt.Location = new System.Drawing.Point(26, 174);
+            this.btnNoviKontakt.Name = "btnNoviKontakt";
+            this.btnNoviKontakt.Size = new System.Drawing.Size(75, 23);
+            this.btnNoviKontakt.TabIndex = 15;
+            this.btnNoviKontakt.Text = "Novi kontakt";
+            this.btnNoviKontakt.UseVisualStyleBackColor = true;
+            // 
+            // btnPretrazi
+            // 
+            this.btnPretrazi.Location = new System.Drawing.Point(257, 304);
+            this.btnPretrazi.Name = "btnPretrazi";
+            this.btnPretrazi.Size = new System.Drawing.Size(75, 23);
+            this.btnPretrazi.TabIndex = 16;
+            this.btnPretrazi.Text = "Pretrazi";
+            this.btnPretrazi.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbGrupa);
+            this.groupBox1.Controls.Add(this.btnNoviKontakt);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnSpremi);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtIme);
+            this.groupBox1.Controls.Add(this.txtBroj);
+            this.groupBox1.Location = new System.Drawing.Point(368, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(332, 209);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Osoba";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 420);
+            this.Controls.Add(this.btnPretrazi);
+            this.Controls.Add(this.txtPretrazi);
+            this.Controls.Add(this.lbKontakta);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtBroj;
+        private System.Windows.Forms.ComboBox cmbGrupa;
+        private System.Windows.Forms.ListBox lbKontakta;
+        private System.Windows.Forms.TextBox txtPretrazi;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnNoviKontakt;
+        private System.Windows.Forms.Button btnPretrazi;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
